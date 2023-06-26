0f776a0e2bec66d3e8796f3dacf09efdcf440ebf
Vishu Bansal <bansalvishu56@gmail.com>
2023-06-26 12:27:31 +0530

    ifna

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,10 +1,16 @@
 import C1 from "../public/Cimg.jpg";
+import C2 from "../public/Cimg(1).jpg";
 import Image from "next/image";
 
 const Carousel = () => {
   return (
     <div>
-      <Image src={C1} className="w-full h-1/2"></Image>
+      <div className="block sm:hidden">
+        <Image src={C1} className="w-full h-1/2"></Image>
+      </div>
+      <div className="hidden sm:block">
+        <Image src={C2} className="w-full h-1/2"></Image>
+      </div>
     </div>
   );
 };
