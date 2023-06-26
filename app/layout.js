ca5a6744b9ba4501db88fc7ce1b3d9f7165d47a3
Vishu Bansal <bansalvishu56@gmail.com>
2023-06-26 11:51:49 +0530

    hydration

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Nav from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Nav />
+    <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Footer />
-    </div>
+    </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,14 +3,18 @@ import Carousel from "@/components/Carousel";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
+import Nav from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
+      <Nav />
       <Carousel />
       <About />
       <Services />
-      = <Contact />={" "}
+      <Contact />
+      <Footer />
     </main>
   );
 }
